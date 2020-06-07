ba7396483504ec73aef3e15494fa1880ae7c9631
wilfru <fruwilliams@gmail.com>
2020-06-07 16:58:45 +0300

    myscript_wilfru

---
+++ scripts/Williamson_Fru.js
@@ -0,0 +1,6 @@
+function wil(fullname="Williamson Fru", id ="HNG-06733", mail ="fruwilliams@gmail.com", language ="JavaScript"){
+    console.log(
+     `Hello World, this is ${fullname} with HNGi7 ID ${id} and email ${mail} using ${language} for stage 2 task `
+    );
+}
+wil();
